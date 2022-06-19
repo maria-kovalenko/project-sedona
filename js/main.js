6344bdc58eb6f685069744490c03cb7df6f22404
Maria <m1a2r3.kovalenko@gmail.com>
2022-06-19 14:11:45 +0300

    доработан скрипт открытия навигации

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
-var buttonOpen = document.querySelector(".page-header__toggle--opened");
-var buttonClose = document.querySelector(".page-header__toggle--closed");
-var menu = document.querySelector(".main-nav");
+const buttonOpen = document.querySelector(".page-header__toggle--opened");
+const buttonClose = document.querySelector(".page-header__toggle--closed");
+const menu = document.querySelector(".main-nav");
+
+let condition = "close";
 
 buttonClose.addEventListener("click", function () {
-  buttonClose.classList.remove("page-header__toggle--closed");
-  buttonClose.classList.add("page-header__toggle--opened");
-  menu.classList.add("main-nav-opened");
+  if (condition === "close") {
+    buttonClose.classList.remove("page-header__toggle--closed");
+    buttonClose.classList.add("page-header__toggle--opened");
+    menu.classList.add("main-nav-opened");
+    condition = "open";
+  } else {
+    buttonClose.classList.remove("page-header__toggle--opened");
+    buttonClose.classList.add("page-header__toggle--closed");
+    menu.classList.remove("main-nav-opened");
+    condition = "close";
+  }
 });
